da4efa4505ea1232d7f3eaba6c163b899e6a258d
genki <reguza525@gmail.com>
2021-07-04 23:51:33 +0900

    marged

--- main.py
+++ main.py
@@ -44,10 +44,6 @@ def reply_msg():
     except InvalidSignatureError:
         abort(400)
     return "OK."
-
-@handler.add(MessageEvent,message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
     
 @handler.add(MessageEvent,message=TextMessage)
 def tell_bustime_iki(event):
