097f6414cc0c1e9f0efaf9dac8880b8871dbce3d
genki <reguza525@gmail.com>
2021-07-04 18:17:33 +0900

    aa

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Created on Sat July 4 14:39:49 2021
 
 @author: 木村　元紀
 """
-
+from bs4 import BeautifulSoup
 from flask import Flask,request,abort
 from linebot import (
     LineBotApi,WebhookHandler
@@ -17,6 +17,8 @@ from linebot.models import(
     FollowEvent)
 
 import os
+import requests
+import datetime
 
 app = Flask(__name__)
 
@@ -26,6 +28,7 @@ CHANNEL_SECRET_TOKEN = os.environ['YOUR_CHANNEL_SECRET']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET_TOKEN)
 
+date_now = datetime.now()
 #URL = "https://www.navitime.co.jp/bus/diagram/timelist?hour=3&departure=00031884&arrival=00031140&line=00009702&date={}-{}-{}".format(date_now.year,date_now.month,date_now.day)
 URL = "https://www.navitime.co.jp/bus/diagram/timelist?hour=3&departure=00031884&arrival=00031140&line=00009702&date=2021-07-05"
 
@@ -48,6 +51,59 @@ def reply_msg():
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    
+@handler.add(MessageEvent,message=TextMessage)
+def tell_bustime_iki(event):
+    if event.type == "message":
+        if (event.message.text=="バス")or(event.message.text=="バスの時間")or(event.message.text=="バスの時刻表"):
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="現在片道のみ利用可能。「行き」と入力してください"))
+            
+        if (event.message.text=="いき")or(event.message.text=="行き"):
+
+            cnt_h =0
+            cnt_m =0
+            
+            res = requests.get(URL)
+            soup = BeautifulSoup(res.content,"html.parser")
+            soup = soup.find(class_='left wide-page-mode')
+            soup = soup.find(class_="time-list-frame")
+            Hours = soup.find_all("dt")
+            
+            for i in Hours:
+                i = i.text
+                i = i.replace("時","")
+                i = int(i)
+                
+                if (i<date_now.hour):
+                    cnt_h+=1
+                else:
+                    break
+                
+            time = Hours[cnt_h].text
+            time = time.replace("時","")
+            
+            Mins = soup.find_all("dd")
+            Mins = Mins[cnt_h].find("ol")
+            Mins = Mins.find_all(class_="time-detail")
+            
+            for j in Mins:
+                Min = j.find(class_="time dep")
+                Min = Min.text
+                Min = Min.replace(time+":","")
+                Min = int(Min)
+                
+                if(Min<date_now.minute):
+                    cnt_m+=1
+                else:
+                    break
+                
+            TIME_d = Mins[cnt_m].find(class_="time dep")
+            TIME_d = TIME_d.text
+            TIME_a = Mins[cnt_m].find(class_="time arr")
+            TIME_a = TIME_a.text
+            TEXT_BT = TIME_d+"=>"+TIME_a
+    
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(TEXT_BT))
 
         
 if __name__ == '__main__':
